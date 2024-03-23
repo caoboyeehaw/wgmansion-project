378fe4099a29b4c4a8729d561904b8102afbf88f
mechamsounds <james.lee.uh@gmail.com>
2024-03-22 21:46:31 -0500

    token generator tests

---
+++ WGMansion.Api/WGMansion.Api.UnitTests/Utilities/TokenGeneratorTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Options;
+using Moq;
+using WGMansion.Api.Models;
+using WGMansion.Api.Settings;
+using WGMansion.Api.Utility;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.IdentityModel.Tokens.Jwt;
+using MongoDB.Driver.Linq;
+
+namespace WGMansion.Api.UnitTests.Utilities
+{
+    [TestFixture]
+    internal class TokenGeneratorTests
+    {
+        private TokenGenerator _sut;
+        private Mock<IOptions<AppSettings>> _appSettings = new Mock<IOptions<AppSettings>>();
+
+        [SetUp]
+        public void Setup()
+        {
+            var appSettings = new AppSettings
+            {
+                Secret = "secret secret secret secret secret secret secret secret secret secret "
+            };
+
+            _appSettings.Setup(x => x.Value).Returns(appSettings);
+
+            _sut = new TokenGenerator(_appSettings.Object);
+        }
+
+        [Test]
+        public void TestGenerateToken()
+        {
+            var account = new Account
+            {
+                Id = new ObjectId(),
+                UserRole = "Admin"
+            };
+
+            var result = _sut.GetToken(account);
+            var token = new JwtSecurityToken(result);
+            var date = DateTime.UtcNow.AddDays(7);
+
+            Assert.That(result,Is.Not.Null);
+            Assert.That(token.Claims.First(x => x.Type == "role").Value, Is.EquivalentTo("Admin"));
+            Assert.That(token.ValidTo.Date, Is.EqualTo(date.Date));
+        }
+    }
+}
