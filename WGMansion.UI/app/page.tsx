c4474facc7faba20febee32cfabcd92c9362424f
caoboy! <70660848+caoboyeehaw@users.noreply.github.com>
2024-03-26 12:52:46 -0700

    deleted the old page

--- WGMansion.UI/app/page.tsx
+++ WGMansion.UI/app/page.tsx
@@ -240,19 +240,4 @@ export default function Home() {
 }
 
 
-/*
-export default function Home() {
-  return (
-    <div className="justify-between flex">
-      <div className="items-center flex">wargame mansion app</div>
-      <Link href="/test">
-        <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Go to Test Page
-        </div>
-      </Link>
-    </div>
-  );
-}
-*/
-
 
